ce4cf316b685ca6476607cd3c6f37f46fb57e962
RiveraHan <hanzellrivera95@gmail.com>
2024-01-21 14:35:34 -0600

    Finished tutorial (#10)
    * Added route dynamics
    
    * Fixed function getStaticPaths()
    Added types
    
    * Added feed rss
    Added index tags
    Added packages rss
    
    * Added Theme dark
    Added framework UI
    Finished tutorial
    
    ---------
    
    Co-authored-by: Hanzell Rivera <hanzellrivera95@email.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://hanzellriverablog.vercel.app"
+  site: "https://hanzellriverablog.vercel.app",
+  integrations: [preact()]
 });
